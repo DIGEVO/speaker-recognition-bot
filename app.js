0ed57e93a8beb16b3b12cb7e133b3c0c00384ac8
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-03 16:20:54 -0500

    testing

--- app.js
+++ app.js
@@ -5,40 +5,11 @@ const utils = require('./utils');
 
 const bot = utils.initBot();
 
-bot.dialog('/', (session) =>
-    session.send(`Hola ${session.message.user.name.split(" ", 1)[0]}, me dijiste: ${session.message.text}`));
+bot.dialog('/', (session) => {
+    session.send(`Hola ${getName(session.message)}, me dijiste: ${JSON.stringify(session.message)}`)
+});
 
-// /*-----------------------------------------------------------------------------
-// A simple echo bot for the Microsoft Bot Framework. 
-// -----------------------------------------------------------------------------*/
 
-// var restify = require('restify');
-// var builder = require('botbuilder');
-
-// // Setup Restify Server
-// var server = restify.createServer();
-// server.listen(process.env.port || process.env.PORT || 3978, function () {
-//    console.log('%s listening to %s', server.name, server.url); 
-// });
-  
-// // Create chat connector for communicating with the Bot Framework Service
-// var connector = new builder.ChatConnector({
-//     appId: process.env.MicrosoftAppId,
-//     appPassword: process.env.MicrosoftAppPassword,
-//     stateEndpoint: process.env.BotStateEndpoint,
-//     openIdMetadata: process.env.BotOpenIdMetadata 
-// });
-
-// // Listen for messages from users 
-// server.post('/api/messages', connector.listen());
-
-// /*----------------------------------------------------------------------------------------
-// * Bot Storage: This is a great spot to register the private state storage for your bot. 
-// * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-// * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-// * ---------------------------------------------------------------------------------------- */
-
-// // Create your bot with a function to receive messages from the user
-// var bot = new builder.UniversalBot(connector, function (session) {
-//     session.send("You said: %s", session.message.text);
-// });
+function getName(message) {
+    return message.user.name.split(" ", 1)[0];
+}
