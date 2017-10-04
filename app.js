d34ca5605e35d0c5af953970eec9601f87cbc80e
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-04 09:50:01 -0500

    testing

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 'use strict';
 
 const builder = require('botbuilder');
+const request = require('request');
+
 const utils = require('./utils');
 
 const bot = utils.initBot();
 
 bot.dialog('/', (session) => {
 
-    const txt = session.message.attachments.map(att => JSON.stringify(att)).join('<->');//  reduce((acc, att) => acc.concat(JSON.stringify(att), "<->"),"");
-
+    const txt = session.message.attachments
+        .map(att => JSON.stringify(att)).join('<->');
+    //session.message.address.channelId
     session.send(txt);
+
+    if (session.message.attachments.length > 0) {
+        const attachment = msg.attachments[0];
+
+        session.send({
+            text: "You sent:",
+            attachments: [
+                {
+                    contentType: attachment.contentType,
+                    contentUrl: attachment.contentUrl,
+                    name: attachment.name
+                }
+            ]
+        });
+        // const url = session.message.attachments[0].contentUrl;
+        // request.get(url, (error, response, body) => {
+        //     if (!error && response.statusCode == 200) {
+        //         var csv = body;
+        //         // Continue with your processing here.
+
+        //     }
+        // });
+
+    } else {
+        session.send("You said: %s", session.message.text);
+    }
+
+
 });
 
 
